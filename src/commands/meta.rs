390fed90052f127e5884fef321743ba70b4bd007
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-10-12 12:19:13 -0400

    Add sources command

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,12 +1,18 @@
 use crate::{embed_helper::Embed, Context, Error};
 
-#[poise::command(slash_command, prefix_command, subcommands("calculations"))]
+#[poise::command(slash_command, prefix_command, subcommands("calculations", "sources"))]
 pub async fn meta(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn calculations(ctx: Context<'_>) -> Result<(), Error> {
-    Embed::from_file("calculations.json").send(ctx).await?;
+    Embed::from_file("meta/calculations.json").send(ctx).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn sources(ctx: Context<'_>) -> Result<(), Error> {
+    Embed::from_file("meta/sources.json").send(ctx).await?;
     Ok(())
 }
